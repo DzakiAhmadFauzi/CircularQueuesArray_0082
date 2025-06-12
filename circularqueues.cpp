0c16d75066a760e3eb892e07491b8dce5d1ad6ff
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-06-12 18:55:50 +0700

    commit 13

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -108,4 +108,16 @@ void display()
             FRONT_position++;
         }
 
-        
+        FRONT_position = 0;
+
+        // Iterasi dari awal array hingga REAR
+        while (FRONT_position <= REAR_position)
+        {
+            cout << queue_array[FRONT_position] << "  ";
+            FRONT_position++;
+        }
+        cout << endl;
+    }
+}
+
+
