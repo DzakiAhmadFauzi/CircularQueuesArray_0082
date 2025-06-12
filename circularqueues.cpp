1d735b57aeb5313bf076aefad78fe250609cef95
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-06-12 18:17:24 +0700

    commit 6

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -40,4 +40,12 @@ public:
         }
         else
         {
-            
+            // Jika REAR sudah berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+    }
+    queue_array[REAR] = num;
+}
+
