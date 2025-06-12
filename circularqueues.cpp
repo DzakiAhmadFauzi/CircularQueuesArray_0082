914769c046b2f6f2e5d5352c2754ca3c38718ce6
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-06-12 18:57:24 +0700

    commit 16

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -139,4 +139,17 @@ int main()
             cin >> ch;
             cout << endl;
 
+            switch (ch)
+            {
+            case '1':
+            {
+
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
             
